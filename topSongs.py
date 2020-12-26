97749fdefcb74b9a4c942a2657cf3c66043a8505
prathit <prathit@umich.edu>
2020-12-26 15:46:50 -0500

    revamped script with command line options and output as python dictionary

--- topSongs.py
+++ topSongs.py
@@ -1,16 +1,21 @@
+import sys
+import os
+import errno
 import billboard
 
-def getSongs(pastYears = 10, chartTitle = 'hot-100-songs', yearStart = 2019):
+
+def getSongs(pastYears = 10, chartTitle = 'hot-100-songs', yearStart = 2019, filename = "songObj.py"):
     # looping through ChartData objects to store the data in a large dictionary
     # storing song title, artist, ranking, and year 
     # currently year end charts are only supported from 2015
+    flags = os.O_CREAT | os.O_EXCL | os.O_WRONLY
     print(yearStart)
     songDict = {}
     # structure of songDict
     # Key  is struct of title and artists accesible by dot notation
     # Value is tuple of (year, rank)
     for year in range(yearStart,yearStart-pastYears,-1):
-        #print(year)
+        print(year)
 
     
         
@@ -28,9 +33,43 @@ def getSongs(pastYears = 10, chartTitle = 'hot-100-songs', yearStart = 2019):
                     songDict[songTuple] = (year,rank)
             else:
                 songDict[songTuple] = (year,rank)
-    #print(len(songDict))
-    pp = pprint.PrettyPrinter(indent=4, compact=True)
-    pp.pprint(songDict)
+
+
+    try:
+        file_handle = os.open(filename, flags)
+    except OSError as e:
+        if e.errno == errno.EEXIST:  # Failed as the file already exists.
+            print("File already Exists")
+            pass
+        else:  # Something unexpected went wrong so reraise the exception.
+            raise
+    else:  # No exception, so the file must have been created successfully.
+        with os.fdopen(file_handle, 'w') as file_obj:
+        # Using `os.fdopen` converts the handle to an object that acts like a
+        # regular Python file object, and the `with` context manager means the
+        # file will be automatically closed when we're done with it.
+            file_obj.write("songDictionary = ")
+            
+            file_obj.write(str(songDict))
+    
+    
     return songDict
+
+
+# running as script
 if __name__ == "__main__":
-    getSongs(3)
+    print(f"Arguments count: {len(sys.argv)}")
+    if "-h" in sys.argv or "--help" in sys.argv or len(sys.argv)==1:
+        print('python3 test.py <outputFile> <yearStart> <numPastYears> <chart-title> \nNote: outputFile is required, only year-end charts are supported')
+        print("Year End Chart Names: ")
+        print(billboard.charts(year_end=True))
+        sys.exit(1)
+    if len(sys.argv)==2:
+        getSongs(filename = sys.argv[1])
+    if len(sys.argv)==3:
+        getSongs(filename = sys.argv[1],yearStart=int(sys.argv[2]))
+    if len(sys.argv)==4:
+        getSongs(filename = sys.argv[1],yearStart=int(sys.argv[2]),pastYears=int(sys.argv[3]))
+    if len(sys.argv)==5:
+        getSongs(filename = sys.argv[1],yearStart=int(sys.argv[2]),pastYears=int(sys.argv[3]),chartTitle=sys.argv[4])
+
