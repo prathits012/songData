a7f9dc8f553d1b19d388796b16c889ea41810283
mgagin <mgagin@umich.edu>
2020-12-31 23:05:44 -0500

    Finished retrieving genre info from Genius (primary and general) and added misc. things

---
+++ random-ideas.py
@@ -0,0 +1,11 @@
+'''
+Conglomerate dictionary that encompasses all the songs we parse through:
+    Key: Word (lyric)
+    Value: List (first time a song has used this lyric(by billboard date))
+
+Song dictionary
+    Key: Day
+    Value: List of songs (title, artist, URI)
+        Can use last bit of URI *********:abcd as ID for lookup
+'''
+
--- spotifyCsvToDict.py
+++ spotifyCsvToDict.py
@@ -8,39 +8,21 @@ flags = os.O_CREAT | os.O_EXCL | os.O_WRONLY
 with open('output1.txt', newline='') as csvfile:
     rowReader = csv.reader(csvfile)
 
-      
-    # extracting field names through first row 
-    
+
+    # extracting field names through first row
+
     fields = next(rowReader)
     currentRow = None
     for row in rowReader:
         #print(row[-1])
         if row[-1] not in spotifyDict:
             # adding a new date to the dictionary
-            spotifyDict[row[-1]] = [{"title":row[2],"artist":row[3]}]
+            spotifyDict[row[-1]] = [{"title":row[2],"artist":row[3],"id":row[5][31:]}]
         else:
-            spotifyDict[row[-1]].append([{"title":row[2],"artist":row[3]}])
-        
+            spotifyDict[row[-1]].append([{"title":row[2],"artist":row[3],"id":row[5][31:]}])
+
         filename = "song_dict_spotify.py"
 
 
-    # writing to file if file doesn't exist
-try:
-    file_handle = os.open(filename, flags)
-except OSError as e:
-    if e.errno == errno.EEXIST:  # Failed as the file already exists.
-        print("File already Exists")
-        pass
-    else:  # Something unexpected went wrong so reraise the exception.
-        raise
-else:  # No exception, so the file must have been created successfully.
-    with os.fdopen(file_handle, 'w') as file_obj:
-    # Using `os.fdopen` converts the handle to an object that acts like a
-    # regular Python file object, and the `with` context manager means the
-    # file will be automatically closed when we're done with it.
-        file_obj.write("spotifyDictionary = ")
-        
-        file_obj.write(str(spotifyDict))
-    
-    
-    
+with open(filename, 'w') as outfile:
+    outfile.write(f"spotifyDictionary = {str(spotifyDict)}")
