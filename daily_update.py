efac79f3cf13ee4f037849f96cf9c987dca84ce5
mgagin <mgagin@umich.edu>
2021-01-07 00:32:28 -0500

    Changing f string format

--- daily_update.py
+++ daily_update.py
@@ -9,7 +9,7 @@ yesterday = datetime.strftime(yesterday, '%Y-%m-%d')
 a_year_ago = datetime.now() - timedelta(365)
 a_year_ago = datetime.strftime(a_year_ago, '%Y-%m-%d')
 
-os.system("python3 processing/charts.py --START-DATE {} --END-DATE {}".format(a_year_ago ,yesterday,))
+os.system(f"python3 processing/charts.py --START-DATE {a_year_ago} --END-DATE {yesterday}")
 
 os.system("python3 processing/spotifyCsvToDict.py")
 
